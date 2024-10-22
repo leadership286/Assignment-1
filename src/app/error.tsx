fd9432329f0a9a41a76d7c19cc6d3180128f7f5a
Rimsha Ansari <159952102+leadership286@users.noreply.github.com>
2024-10-22 05:34:03 +0500

    Add files via upload

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+
+'use client';
+
+export default function Error() {
+    return (
+        <div className="bg-fuchsia-500 min-h-screen flex items-center justify-center text-4xl text-center p-4">
+            Oops! This page is not implemented yet. It’s like a house with no walls—still a work in progress!
+        </div>
+    );
+}
