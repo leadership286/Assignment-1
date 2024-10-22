62201e0985bf644e909556426cc69f1895e53e39
Rimsha Ansari <159952102+leadership286@users.noreply.github.com>
2024-10-22 05:34:49 +0500

    Add files via upload

---
+++ src/app/FAQs/page.tsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+export default function FAQs(){
+    
+    throw new Error("Simulated error on Example Page!");
+    return <div>This text will not be displayed due to the error.</div>;
+
+
+}
