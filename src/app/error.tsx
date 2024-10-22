8f1ff6b535c0514a41a86307ab1b8036fe99beac
Rimsha Ansari <159952102+leadership286@users.noreply.github.com>
2024-10-22 05:31:50 +0500

    Delete src/app/error.tsx

--- src/app/error.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-export default function Error(){
-    return(
-        <div className="bg-fuchsia-500 min-h-screen text-4xl ">
-            "Oops! This page is not implemented yet. It’s like a house with no walls—still a work in progress!"
-            </div>
-    )
-}
